126c98ef4b4cfb5911bee7d9b2c41baa354482db
Geordeaux <52765543+Geordeaux@users.noreply.github.com>
2025-11-20 10:10:13 +1100

    Remove unused files and update repo

--- inspect_spacing.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-with open('/Users/raphaeleastment/Desktop/ScannerDetections/rules/atlassian/rules/atlassian_addon_installed.yml', 'r') as f:
-    data = yaml.safe_load(f)
-
-print(f"Description repr: {repr(data['description'])}")
-print(f"Query text repr: {repr(data['query_text'])}")
